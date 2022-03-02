53af2b67f824932bf825cf9fdd15b97bd39d688d
T360-v2-DTPB-s10 <csongor.szecsenyi@telekom.com>
2022-03-02 16:19:04 +0100

    For ciklus

---
+++ control_loop.py
@@ -0,0 +1,18 @@
+print("Begin")
+
+name = "John Doe"
+print(name)
+name = "Jack Doe"
+
+
+#count = 5
+#for i in range(0,count):
+#    print(i)
+#print("Hello Python!")
+
+for name in ["john", "jack", "jane"]:
+    print(name)
+
+for i in [2,4,6,8,10]:
+    print(i)
+
---
+++ name_repeater.py
@@ -0,0 +1,5 @@
+name = input("Neved: ")
+count = int(input("Hányszor írja ki?"))
+
+for i in range (1,count+1):
+    print(str(i) + " " + name)
