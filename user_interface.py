7f8ba2b495f73e33c0a2bbd89f662ed601fbc783
T360-v2-DTPB-s10 <csongor.szecsenyi@telekom.com>
2022-03-02 15:33:08 +0100

    Input

---
+++ user_interface.py
@@ -0,0 +1,7 @@
+#name = input("Add meg a neved!")
+#print(name)
+
+number = input("Adj meg egy számot, és én megszorzom öttel")
+print(int(number)*5)
+
+#print(int("abc")) # ValueError: invalid literal for int() with base 10: 'abc'
