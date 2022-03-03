76f0d251c545636d6e465933f78a24d1aed8eaad
T360-v2-DTPB-s10 <csongor.szecsenyi@telekom.com>
2022-03-03 09:23:57 +0100

    For ciklus

---
+++ sum.py
@@ -0,0 +1,12 @@
+sum = 0
+numbers = [5,6,9,10,25]
+for i in range(len(numbers)):
+    sum +=numbers[i]
+
+print(sum)
+
+# sum = 0
+# numbers = [5, 7, 2, 1]
+# for number in numbers:
+#     sum += number
+# print(sum)
